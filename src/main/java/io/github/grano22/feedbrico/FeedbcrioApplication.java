3b859d3946d4e749ff4cb067e8ec8617ca8c681d
unknown <grano22@outlook.com>
2025-10-18 13:48:27 +0200

    Initial codebase

---
+++ src/main/java/io/github/grano22/feedbrico/FeedbcrioApplication.java
@@ -0,0 +1,13 @@
+package io.github.grano22.feedbrico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbcrioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedbcrioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/grano22/feedbrico/FeedbcrioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.grano22.feedbrico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbcrioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
